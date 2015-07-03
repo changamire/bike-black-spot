3b6c8a808a899f771787b0764ec8912ed62c91d8
Claire Dodd <cdodd@thoughtworks.com>
2015-07-03 11:10:37 +1000

    Finished setting up very basic routing

--- routes/admin.rb
+++ routes/admin.rb
@@ -4,6 +4,17 @@ post '/recipient' do
   # params: name, email, location, category
 end
 
+update '/recipient' do
+
+end
+
+get '/recipient' do
+
+end
+
 # get '/' do
 # #   Root.new.get(params)
 # # end
+post 'category' do
+
+end
