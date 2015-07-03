bbfa83e27dcf2202d0b7e082f505d6d3d0e75821
Andrew Watson <awatson@thoughtworks.com>
2015-07-03 15:04:34 +1000

    remove db config from api_log.rb

--- db/schema/api_log.rb
+++ db/schema/api_log.rb
@@ -2,7 +2,7 @@ require 'mongoid'
 
 class ApiLog
   include Mongoid::Document
-  store_in collection: 'api_logs', database: 'bike-black-spot', session: 'default'
+  store_in collection: 'api_logs'
 
   #Example: 127.0.0.1 user-identifier frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326
   field :ip_address, type: String
