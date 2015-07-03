bf63cd1a311ed7e8d2e54849bc2153ad1b8fb714
Claire Dodd <cdodd@thoughtworks.com>
2015-07-03 10:15:18 +1000

    cleaned up app.rb file to remove old unused code

--- app.rb
+++ app.rb
@@ -11,35 +11,4 @@ class BikeSpot < Sinatra::Base
 
   #Load DB Config
   Mongoid.load!('mongoid.yml')
-
-
-
-  #TESTING
-  get '/add-data' do
-    Humidity.create(date: Time.now, reading: 46.8)
-    Temperature.create(date: Time.now, reading: 87.2)
-    'Great! You triggered fake data creation!'
-  end
-
-  get '/data.json' do
-    content_type :json
-    all_data = Temperature.all + Humidity.all
-    all_data.to_json
-  end
-  #END TESTING
-
-end
-
-class Humidity
-  include Mongoid::Document
-
-  field :date, type: Time
-  field :reading, type: Float
-end
-
-class Temperature
-  include Mongoid::Document
-
-  field :date, type: Time
-  field :reading, type: Float
 end
