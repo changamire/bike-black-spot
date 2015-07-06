66958dace1add1ce64d3537a9ece626d22f255d0
Liam Ketchup <lketchup@thoughtworks.com>
2015-07-06 14:03:10 +1000

    User test stub

--- test/routes/user_spec.rb
+++ test/routes/user_spec.rb
@@ -11,6 +11,16 @@ describe 'user' do
     Sinatra::Application
   end
 
+  describe 'Post to /user' do
+    # it 'should have have params'
+    # it 'should handle optional params'
+    # it 'should return 200 (OK)'
+    # it 'should return 500 on errors'
+    # it 'should return UUID'
+    # it 'should send email'
+    # it 'should create user in db'
+  end
+
   it 'root / should return success code' do
     get '/'
     check_last_response_is_ok
