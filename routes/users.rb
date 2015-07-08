7ede88a8d603b7134fef0e18f4c8e31f7421eee6
Frenchie <stewarts@thoughtworks.com>
2015-07-08 11:16:26 +1000

    write to db works

--- routes/users.rb
+++ routes/users.rb
@@ -1,13 +1,12 @@
 post '/users' do
-  if params['name'].nil? and params['email'].nil?
-    return status 500
-  end
-	u = User.new(params[:name])
-	u.save!
-	status 200
-	u.to_json
+  # if params['name'].nil? and params['email'].nil?
+  #   return status 500
+  # end
+	@u = User.create(params)
+	@u.to_json
 end
 
 post '/users/confirm' do
   "user confirm page"
 end
+
