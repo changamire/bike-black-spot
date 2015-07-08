bca5a42c3206491a40caa5621cda95ecb36f668c
Frenchie <stewarts@thoughtworks.com>
2015-07-08 12:35:53 +1000

    WIP: PG config for test

--- environments.rb
+++ environments.rb
@@ -3,7 +3,20 @@ configure :development do
  	set :show_exceptions, true
 end
 
-configure :production, :test do
+
+configure :test do
+ db = URI.parse(ENV['SNAP_DB_PG_URL_ALT')
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
+
+configure :production do
  db = URI.parse(ENV['DATABASE_URL'])
  ActiveRecord::Base.establish_connection(
    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
