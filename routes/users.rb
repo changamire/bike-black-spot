d7c8bb3011edcefb874fba43a2be9a28e6727619
Frenchie <stewarts@thoughtworks.com>
2015-07-08 10:44:39 +1000

    test db write

--- routes/users.rb
+++ routes/users.rb
@@ -2,7 +2,10 @@ post '/user' do
   if params['name'].nil? and params['email'].nil?
     return status 500
   end
-  SecureRandom.uuid
+	u = User.new(params[:name])
+	u.save!
+	status 200
+	u.to_json
 end
 
 post '/user/confirm' do
