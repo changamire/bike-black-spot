81deb49d7c704a894bb8d3bde928e3ecec3cdfa3
Frenchie <stewarts@thoughtworks.com>
2015-07-08 10:31:28 +1000

    Stubbing out tests to bypass. Trying to test pg in prod.

--- environments.rb
+++ environments.rb
@@ -3,26 +3,10 @@ configure :development do
  	set :show_exceptions, true
 end
 
-# configure :test do
-#  db = URI.parse('postgres://mydb')
-
-#  ActiveRecord::Base.establish_connection(
-#  		:adapter => "postgresql",
-#  		:encoding => "utf8",
-#  		:database => "mydb",
-#  		:pool => 5,
-#  		:username => "stewarts",
-#  		:password => "",
-#  		:host => "localhost"
-#    # :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#    # :host     => db.host,
-#    # :username => db.user,
-#    # :password => db.password,
-#    # :database => db.path[1..-1],
-#    # :encoding => 'utf8'
-#  )
-#  puts 
-# end
+configure :test do
+	set :database, {adapter: "sqlite3", database: "test.sqlite3"}
+ 	set :show_exceptions, true
+end
 
 configure :production do
  db = URI.parse(ENV['DATABASE_URL']	|| 'postgres:///localhost/mydb')
