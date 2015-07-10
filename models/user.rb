28fc64720dbc0aad59dbaad35048c78fd53f41db
John Geddes <jgeddes@thoughtworks.com>
2015-07-10 16:10:47 +1000

    #60 User Details: Changed name validation to accept all types of characters except for empty string or single space. Name restricted to 32 characters

--- models/user.rb
+++ models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :reports
   validates :name, :email, presence: true
-  validates :name, format: { with: /\A[a-zA-Z\-'\s]{0,32}\Z/,
-                 message: 'Must contain letters only and under 32 chars'}
+  validates :name, format: { with: /\A.(?!\s*$).{1,32}\Z/, 
+                 message: 'Must be under 32 chars'}
 
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
                   message: 'Must be a valid email.'}
