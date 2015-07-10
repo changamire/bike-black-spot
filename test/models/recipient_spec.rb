e847958029251787998afede08f9ceb0f7d7dc51
Liam Ketchup <lketchup@thoughtworks.com>
2015-07-10 15:49:55 +1000

    UUID generated on Recipient create

--- test/models/recipient_spec.rb
+++ test/models/recipient_spec.rb
@@ -1,13 +1,21 @@
 require_relative '../spec_helper'
 
 describe 'Recipient' do
-  # This really should have Model unit tests, on save, etc
-
-  r = Recipient.new
 
   describe 'show message' do
     it 'should return default message when called' do
+      r = Recipient.new
       expect(r.show_message).to eq(Recipient::DEFAULT_MESSAGE)
     end
   end
+
+  describe 'on create' do
+
+    it 'should generate uuid' do
+      r = Recipient.create
+      expect(r.uuid).to match(/[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/)
+    end
+
+  end
+
 end
