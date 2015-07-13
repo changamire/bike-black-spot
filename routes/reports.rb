0d30629241aac674b349ae9cf40bb636619e2980
Claire Dodd <cdodd@thoughtworks.com>
2015-07-13 12:36:35 +1000

    Added create reports params validation

--- routes/reports.rb
+++ routes/reports.rb
@@ -1,19 +1,30 @@
 require_relative 'routing_locations'
+require_relative '../helpers/param_validation_helper'
 
 get RoutingLocations::REPORTS + '/?' do
-  status 200
-  # Report.all.to_json
+  return status 500 unless validate_get_params?(params)
+
+  return Report.find_by(uuid: params[:uuid]).to_json unless params[:uuid].nil?
+
+  return Report.all.to_json
 end
 
-get RoutingLocations::REPORTS + '/:uuid' do
-  # r = Report.find(params[:uuid])
-  # r.to_json
-end
 
 post RoutingLocations::REPORTS do
-  # r = Response.new(user: params[:uuid], location: params[:location],
-  # 										notes: params[:notes], image: param[:image])
-  r.create
-  status 201
-  # r.to_json
+  return status 500 unless validate_post_params?(params)
+  status 200
 end
+
+
+def validate_get_params?(params)
+  permitted = %w(uuid)
+  required = %w()
+  params_permitted?(params, permitted) && params_required?(params, required)
+end
+
+def validate_post_params?(params)
+  permitted = %w(uuid lat long category description)
+  required = %w(uuid lat long category)
+  params_permitted?(params, permitted) && params_required?(params, required)
+end
+
--- test/routes/reports_spec.rb
+++ test/routes/reports_spec.rb
@@ -3,32 +3,60 @@ require_relative '../spec_helper'
 describe 'Reports' do
   describe 'Get /reports' do
 
-    # TODO: Find a way to mock database
-    xit 'should return all reports on no params' do
-      Report.create(user: 'one')
-      Report.create(user: 'two')
+    it 'should return all reports on no params' do
+      Report.create(lat: '90', long: '90')
+      Report.create(lat: '-90', long: '-90')
 
       get '/reports'
       response = JSON.parse(last_response.body)
-      expect(response[0]['user']).to eq('one')
-      expect(response[1]['user']).to eq('two')
+      expect(response[0]['lat']).to eq('90')
+      expect(response[1]['lat']).to eq('-90')
     end
 
+    it 'should return correct report given uuid' do
+      Report.create(lat: '90', long: '90')
+      expected_report = Report.create(lat: '-90', long: '-90')
+
+      get "/reports?uuid=#{expected_report.uuid}"
+      response = JSON.parse(last_response.body)
+      expect(response['lat']).to eq(expected_report.lat)
+    end
+
+    it 'should return 500 if incorrect params' do
+      get '/reports?fail=something'
+      expect(last_response.status).to eq(500)
+    end
+
+    it 'should return 500 if incorrect params' do
+      get '/reports?uuid=blah&fail=something'
+      expect(last_response.status).to eq(500)
+    end
   end
 
+  describe 'Post /reports' do
+    valid_lat = '85'
+    valid_long = '150'
+    valid_description = 'here is my lovely valid description.'
 
-  xit ' /reports should return success code' do
-    get '/reports'
-    check_last_response_is_ok
-  end
-
-  xit 'get /reports/:report_id should return success code' do
-    get '/reports?report_id=1'
-    check_last_response_is_ok
-  end
-
-  xit 'get /reports?report_id=1,full_report=true should return success code' do
-    get '/reports', params={report_id:1, full_report:true}
-    check_last_response_is_ok
+    params = {}
+    before(:each) do
+      user = User.create(name: 'Tom')
+      category = Category.create(name: 'category1')
+      params = {uuid: user.uuid, lat: valid_lat, long: valid_long, category: category, description: valid_description}
+    end
+    it 'should return status 200(OK) on valid params'do
+      post '/reports', params
+      expect(last_response.status).to eq(200)
+    end
+    it 'should return status 500 on invalid params' do
+      params['fail'] = 'fail'
+      post '/reports', params
+      expect(last_response.status).to eq(500)
+    end
+    it 'should return status 500 without correct params' do
+      category = Category.create(name: 'category1')
+      post '/reports', {lat: valid_lat, long: valid_long, category: category, description: valid_description}
+      expect(last_response.status).to eq(500)
+    end
   end
 end
