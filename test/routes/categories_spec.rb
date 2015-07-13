acb92af716a6eeda4e1e3282dc6c60c4d026ae8d
Claire Dodd <cdodd@thoughtworks.com>
2015-07-14 09:04:50 +1000

    Made categories spec a bit easier to read

--- test/routes/categories_spec.rb
+++ test/routes/categories_spec.rb
@@ -3,33 +3,35 @@ require 'csv'
 require_relative '../spec_helper'
 require_relative '../../models/category'
 
-describe 'Categories' do
+describe 'Exports' do
   categories = [{name: 'category1'}, {name: 'category2'}, {name: 'category3'}]
 
-  describe 'get '+RoutingLocations::CATEGORIES do
-    it ' should return success code' do
-      get RoutingLocations::CATEGORIES
-      check_last_response_is_ok
-    end
-   end
-
-  describe 'get '+RoutingLocations::CATEGORIES + '.json' do
-    it ' should return categories json' do
-      categories.each do |category|
-        Category.create(name: category[:name])
+  describe 'get' do
+    describe RoutingLocations::CATEGORIES do
+      it 'should return success code' do
+        get RoutingLocations::CATEGORIES
+        check_last_response_is_ok
       end
-      get RoutingLocations::CATEGORIES + '.json'
-      expect(JSON.parse(last_response.body)).to eql(JSON.parse(categories.to_json))
     end
-  end
 
-  describe 'get '+RoutingLocations::CATEGORIES + '.csv' do
-    it ' should return categories csv' do
-      categories.each do |category|
-        Category.create(name: category[:name])
+    describe RoutingLocations::CATEGORIES + '.json' do
+      it 'should return categories json' do
+        categories.each do |category|
+          Category.create(name: category[:name])
+        end
+        get RoutingLocations::CATEGORIES + '.json'
+        expect(JSON.parse(last_response.body)).to eql(JSON.parse(categories.to_json))
       end
-      get RoutingLocations::CATEGORIES + '.csv'
-      expect(CSV.parse(last_response.body)).to eql(CSV.parse(categories.to_csv))
+    end
+
+    describe RoutingLocations::CATEGORIES + '.csv' do
+      it 'should return categories csv' do
+        categories.each do |category|
+          Category.create(name: category[:name])
+        end
+        get RoutingLocations::CATEGORIES + '.csv'
+        expect(CSV.parse(last_response.body)).to eql(CSV.parse(categories.to_csv))
+      end
     end
   end
 end
