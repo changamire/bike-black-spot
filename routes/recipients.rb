a823acc357bb485420d09067c4470868f37a901e
Claire Dodd <cdodd@thoughtworks.com>
2015-07-13 16:43:50 +1000

    Get /recipients by uuid

--- routes/recipients.rb
+++ routes/recipients.rb
@@ -1,5 +1,14 @@
 require_relative 'routing_locations'
 
 get RoutingLocations::RECIPIENTS + '/?' do
+  return status 500 unless validate_get_params?(params)
+  return Recipient.find_by(uuid: params[:uuid]).to_json unless params[:uuid].nil?
   Recipient.all.to_json
+end
+
+
+def validate_get_params?(params)
+  permitted = %w(uuid)
+  required = %w()
+  params_permitted?(params, permitted) && params_required?(params, required)
 end
--- test/routes/recipients_spec.rb
+++ test/routes/recipients_spec.rb
@@ -4,19 +4,41 @@ describe 'Recipients' do
 
   describe 'Get from /recipients' do
 
-    it 'should return return status 200(OK)' do
-      get RoutingLocations::RECIPIENTS
-      expect(last_response).to be_ok
+    describe 'with no params' do
+      it 'should return return status 200(OK)' do
+        get RoutingLocations::RECIPIENTS
+        expect(last_response).to be_ok
+      end
+
+      it 'should return return all recipients' do
+        Recipient.create(name: 'Some Dude', email: 'some@dude.com', lat: '37.8136', long: '144.9631')
+        Recipient.create(name: 'Another Dude', email: 'another@dude.com', lat: '144.9631', long: '37.8136')
+        get RoutingLocations::RECIPIENTS
+        recipients = JSON.parse(last_response.body)
+        expect(recipients[0]['name']).to eq('Some Dude')
+        expect(recipients[1]['name']).to eq('Another Dude')
+      end
     end
 
-    it 'should return return all recipients' do
-      Recipient.create(name: 'Some Dude', email: 'some@dude.com', lat: '37.8136', long: '144.9631')
-      Recipient.create(name: 'Another Dude', email: 'another@dude.com', lat: '144.9631', long: '37.8136')
-      get RoutingLocations::RECIPIENTS
-      recipients = JSON.parse(last_response.body)
-      expect(recipients[0]['name']).to eq('Some Dude')
-      expect(recipients[1]['name']).to eq('Another Dude')
-    end
+    describe 'with params' do
 
+      it 'should return correct recipient' do
+        recipient = Recipient.create(name: 'Another Dude', email: 'another@dude.com', lat: '144.9631', long: '37.8136')
+        get RoutingLocations::RECIPIENTS + "?uuid=#{recipient.uuid}"
+        response = JSON.parse(last_response.body)
+        expect(response['name']).to eq(recipient.name)
+      end
+
+      it 'should return 500 error when incorrect params' do
+        get RoutingLocations::RECIPIENTS + "?uuid=doesntmatterfail&fail=fial"
+        expect(last_response.status).to eq(500)
+      end
+
+      it 'should return 500 error when incorrect params' do
+        get RoutingLocations::RECIPIENTS + "?fail=fial"
+        expect(last_response.status).to eq(500)
+      end
+
+    end
   end
 end
