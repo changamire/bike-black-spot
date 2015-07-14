047d4ffd290b37911e19721ebd5113845d6ec159
Claire Dodd <cdodd@thoughtworks.com>
2015-07-14 15:37:28 +1000

    Fixed unauth redirecting on export not returning status code

--- routes/exports.rb
+++ routes/exports.rb
@@ -4,13 +4,11 @@ require_relative '../models/user'
 require 'csv'
 
 get RoutingLocations::EXPORTS do
+
   permitted = %w(users)
   required = %w()
 
   return status 400 unless validate_params?(params, permitted, required)
-
-  unless (params.has_key?(:users))
-    redirect RoutingLocations::LOGIN unless warden.authenticated?
-    User.export
-  end
+  return status 401 unless warden.authenticated?
+  User.export unless (params.has_key?(:users))
 end
