8c9783bafef86d1070ef5baf7c80406af88fc9c1
Claire Dodd <cdodd@thoughtworks.com>
2015-07-15 12:00:18 +1000

    Refactored recipients slightly

--- routes/recipients.rb
+++ routes/recipients.rb
@@ -9,12 +9,12 @@ get RoutingLocations::RECIPIENTS + '/?' do
 
   return status 400 unless validate_params?(params, permitted, required)
 
-  unless params[:uuid].nil?
-    recipient = Recipient.find_by(uuid: params[:uuid])
-    status 400 if recipient.nil?
-    return recipient.to_json
-  end
-  Recipient.all.to_json
+  return Recipient.all.to_json if params[:uuid].nil?
+
+  recipient = Recipient.find_by(uuid: params[:uuid])
+  status 400 if recipient.nil?
+  return recipient.to_json
+
 end
 
 post RoutingLocations::RECIPIENTS + '/?' do
