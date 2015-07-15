84392df821e7c6d1e4da568f6ee2cf9ff0536778
Claire Dodd <cdodd@thoughtworks.com>
2015-07-15 15:31:52 +1000

    Updated report_spec to be consistent with other specs

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -32,66 +32,70 @@ describe 'Report' do
     it 'Should return a valid report object' do
       expect(Report.first).to_not be_nil
     end
-    it 'Should save uuid to database' do
-      expect(Report.first.uuid).to eq(report.uuid)
-    end
-    it 'Should save latitude into database' do
-      expect(Report.first.lat).to eq(report.lat)
-    end
-    it 'Should save latitude into database' do
-      expect(Report.first.long).to eq(report.long)
-    end
-    it 'Should save latitude into database' do
-      expect(Report.first.description).to eq(report.description)
-    end
-    it 'Should save user reference into database' do
-      expect(Report.first.user_id).to eq(user.id)
-    end
-    it 'Should save category reference into database' do
-      expect(Report.first.category_id).to eq(category.id)
-    end
-  end
+    describe 'should save to database:' do
+      it 'uuid' do
+        expect(Report.first.uuid).to eq(report.uuid)
+      end
+      it 'lat' do
+        expect(Report.first.lat).to eq(report.lat)
+      end
+      it 'long' do
+        expect(Report.first.long).to eq(report.long)
+      end
+      it 'description' do
+        expect(Report.first.description).to eq(report.description)
+      end
+      it 'user reference' do
+        expect(Report.first.user_id).to eq(user.id)
+      end
+      it 'category reference' do
+        expect(Report.first.category_id).to eq(category.id)
+      end
 
-  describe 'validation' do
-    it 'should not allow lat greater than 90' do
-      params['lat'] = '91'
-      report = Report.create(params)
-      expect(report.valid?).to be_falsey
-    end
-    it 'should not allow lat less than -90' do
-      params['lat'] = '-91'
-      report = Report.create(params)
-      expect(report.valid?).to be_falsey
-    end
-    it 'should not allow lat greater than 180' do
-      params['long'] = '-181'
-      report = Report.create(params)
-      expect(report.valid?).to be_falsey
-    end
-    it 'should not allow lat less than -180' do
-      params['long'] = '-181'
-      report = Report.create(params)
-      expect(report.valid?).to be_falsey
-    end
-    it 'should not allow description longer than 500 characters' do
-      description = 'a' * 501
-      params['description'] = description
-      report = Report.create(params)
-      expect(report.valid?).to be_falsey
     end
 
-    it 'should not allow a report with a user that doesn\'t exist' do
-      expect(lambda do
-               params['user'] = 'Random string here woo'
-               Report.create(params)
-             end).to raise_error(ActiveRecord::AssociationTypeMismatch)
-    end
-    it 'should not allow a report with a category that doesn\'t exist' do
-      expect(lambda do
-               params['category'] = 'A category, wow'
-               Report.create(params)
-             end).to raise_error(ActiveRecord::AssociationTypeMismatch)
-    end
+    describe 'validation' do
+      describe 'should not allow' do
+      end
+      it 'lat greater than 90' do
+        params['lat'] = '91'
+        report = Report.create(params)
+        expect(report.valid?).to be_falsey
+      end
+      it 'lat less than -90' do
+        params['lat'] = '-91'
+        report = Report.create(params)
+        expect(report.valid?).to be_falsey
+      end
+      it 'slat greater than 180' do
+        params['long'] = '-181'
+        report = Report.create(params)
+        expect(report.valid?).to be_falsey
+      end
+      it 'lat less than -180' do
+        params['long'] = '-181'
+        report = Report.create(params)
+        expect(report.valid?).to be_falsey
+      end
+      it 'description longer than 500 characters' do
+        description = 'a' * 501
+        params['description'] = description
+        report = Report.create(params)
+        expect(report.valid?).to be_falsey
+      end
 
+      it 'a report with a user that does not exist' do
+        expect(lambda do
+                 params['user'] = 'Random string here woo'
+                 Report.create(params)
+               end).to raise_error(ActiveRecord::AssociationTypeMismatch)
+      end
+      it 'a report with a category that does not exist' do
+        expect(lambda do
+                 params['category'] = 'A category, wow'
+                 Report.create(params)
+               end).to raise_error(ActiveRecord::AssociationTypeMismatch)
+      end
+    end
   end
 end
