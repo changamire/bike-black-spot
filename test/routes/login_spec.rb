18af157c088601f1a38da8799f3d7a7936658fcb
Claire Dodd <cdodd@thoughtworks.com>
2015-07-15 12:23:31 +1000

    login_spec refactored to be consistent with others

--- test/routes/login_spec.rb
+++ test/routes/login_spec.rb
@@ -5,37 +5,40 @@ require_relative '../../models/admin'
 
 describe 'Login' do
   it 'post ' + RoutingLocations::LOGIN + ' with invalid credentials should return status 400' do
-    post RoutingLocations::LOGIN, params={username:'invalid_name',password:'invalid_pw'}
+    post RoutingLocations::LOGIN, params={username: 'invalid_name', password: 'invalid_pw'}
 
     expect(last_response.status).to be(400)
   end
 
   it 'post /login with valid credentials should redirect to /admin' do
-    test_admin = Admin.create(username:'userCat',password:'userPass')
+    test_admin = Admin.create(username: 'userCat', password: 'userPass')
 
-    post RoutingLocations::LOGIN, params={username:'userCat',password:'userPass'}
+    post RoutingLocations::LOGIN, params={username: 'userCat', password: 'userPass'}
 
     expect(last_response.redirect?).to be(true)
-    expect(last_response.location).to eql(LOCAL + RoutingLocations::ADMIN )
+    expect(last_response.location).to eql(LOCAL + RoutingLocations::ADMIN)
 
     test_admin.delete
   end
 end
 
 describe 'Logout' do
-  it 'get '+RoutingLocations::LOGOUT+' should redirect' do
-    get RoutingLocations::LOGOUT
+  describe 'get ' + RoutingLocations::LOGOUT do
+    it 'should redirect' do
+      get RoutingLocations::LOGOUT
 
-    expect(last_response.status).to be(302)
-    expect(last_response.location).to eql(LOCAL + RoutingLocations::ROOT)
-  end
+      expect(last_response.status).to be(302)
+      expect(last_response.location).to eql(LOCAL + RoutingLocations::ROOT)
+    end
 
-  it 'get ' + RoutingLocations::LOGOUT + ' should logout of warden (hitting protected page requires login)' do
-    login_as :Admin
-    get RoutingLocations::LOGOUT
-    expect(last_response.status).to be(302)
-    get RoutingLocations::ADMIN
-    expect(last_response.status).to be(302)
-    expect(last_response.location).to eql(LOCAL + RoutingLocations::LOGIN )
+    it 'should logout of warden (hitting protected page requires login)' do
+      login_as :Admin
+      get RoutingLocations::LOGOUT
+      expect(last_response.status).to be(302)
+      
+      get RoutingLocations::ADMIN
+      expect(last_response.status).to be(302)
+      expect(last_response.location).to eql(LOCAL + RoutingLocations::LOGIN)
+    end
   end
 end
