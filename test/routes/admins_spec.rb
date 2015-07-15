6db2c646433704200432566e15f5fd8b8efbeed4
Claire Dodd <cdodd@thoughtworks.com>
2015-07-15 12:20:41 +1000

    Refactored admin routes tests

--- test/routes/admins_spec.rb
+++ test/routes/admins_spec.rb
@@ -10,21 +10,19 @@ def app
 end
 
 describe 'Admin' do
-  it 'get ' + RoutingLocations::ADMIN + ' should have status 302 when unauthorised' do
-    get RoutingLocations::ADMIN
-    expect(last_response.redirect?).to be(true)
-  end
+  describe 'get' + RoutingLocations::ADMIN  do
+    it 'should redirect to login when unauthorised' do
+      get RoutingLocations::ADMIN
+      expect(last_response.redirect?).to be(true)
+      expect(last_response.location).to eql(LOCAL + RoutingLocations::LOGIN)
+    end
 
-  it 'get ' + RoutingLocations::ADMIN + ' should redirect to login when unauthorised' do
-    get RoutingLocations::ADMIN
-    expect(last_response.location).to eql(LOCAL + RoutingLocations::LOGIN )
-  end
-
-  it 'get ' + RoutingLocations::ADMIN + ' should not redirect when authorised' do
-    login_as :Admin
-    get RoutingLocations::ADMIN
-    expect(last_response.redirect?).to be(false)
-    check_last_response_is_ok
+    it 'should not redirect when authorised' do
+      login_as :Admin
+      get RoutingLocations::ADMIN
+      expect(last_response.redirect?).to be(false)
+      check_last_response_is_ok
+    end
   end
 end
 
