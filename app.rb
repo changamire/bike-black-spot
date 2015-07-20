9d8e77443bb1e7b60d5e326564c0245ed5a488f0
Michael Stafford <mstaffor@thoughtworks.com>
2015-07-20 12:04:00 +1000

    forcing ssl

--- app.rb
+++ app.rb
@@ -4,17 +4,17 @@ require 'sinatra/activerecord'
 require 'warden'
 require 'bcrypt'
 require 'rack/throttle'
+require 'rack/ssl'
 
 require_relative 'app/models/init'
 require_relative 'app/helpers/api_logger.rb'
 require_relative 'app/routes/init'
 require_relative 'app/models/admin'
 require_relative 'app/helpers/bike_spot_warden'
-
 require_relative 'config/environments'
 
-
 use Rack::Throttle::Minute, :max => 1000 if ENV['RACK_ENV'] == 'production'
+use Rack::SSL, :exclude => lambda { |env| ENV['RACK_ENV'] != 'staging' }
 
 class BikeSpot < Sinatra::Base
   register Sinatra::ActiveRecordExtension
