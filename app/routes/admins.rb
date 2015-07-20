07277ae5bcaaf6ec8aaeece9d9be966d5ccc6bcb
John Geddes <jgeddes@thoughtworks.com>
2015-07-20 15:36:05 +1000

    147 Admin view now pulls recipients from db and renders html for table appropriately. WIP: styling, refactoring embedded ruby

--- app/routes/admins.rb
+++ app/routes/admins.rb
@@ -1,6 +1,10 @@
 require_relative 'routing_locations'
+require 'json'
 
 get RoutingLocations::ADMIN  + '/?' do
   return status 401 unless warden.authenticated?
+  
+  @recipients = JSON.parse(Recipient.all.to_json)
+
   erb :admin
 end
