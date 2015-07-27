b55e61992eb2c907108accb21833da636cd643cd
Michael Stafford <mstaffor@thoughtworks.com>
2015-07-27 12:39:26 +1000

    image upload from base64 string to S3 bucket

--- app/helpers/image_upload.rb
+++ app/helpers/image_upload.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk'
+require 'base64'
 
 class ImageUpload
 
@@ -6,12 +7,32 @@ class ImageUpload
       region: ENV['AWS_REGION'],
       credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
   )
+  @bucket_name = ENV['S3_BUCKET']
+  @bucket_folder = 'images'
+  @acl = 'public-read'
 
   def self.upload(file_path, file_name)
     raise 'Invalid File Path.' unless File.exist?(file_path)
-    @obj = @s3.bucket(ENV['S3_BUCKET']).object("#{ENV['RACK_ENV']}/#{file_name}")
-    @obj.upload_file(file_path, acl:'public-read')
-    @obj.public_url
+    obj = @s3.bucket(@bucket_name).object("#{@bucket_folder}/#{file_name}")
+    obj.upload_file(file_path, acl:@acl)
+    obj.public_url
+  end
+
+  def self.upload_base64(image, file_name)
+    File.open(file_name, 'w+') do|f|
+      f.write(Base64.decode64(image))
+    end
+
+    url = upload(file_name,file_name)
+
+    File.delete(file_name)
+
+    return url
+  end
+
+  def self.delete(file_name)
+    obj = @s3.bucket(@bucket_name).object("#{@bucket_folder}/#{file_name}")
+    obj.delete
   end
 
 end
--- spec/helpers/image_upload_spec.rb
+++ spec/helpers/image_upload_spec.rb
@@ -1,23 +1,40 @@
-require_relative '../spec_helper'
 require_relative '../../app/helpers/image_upload'
 require 'open-uri'
+require 'base64'
 
 describe 'ImageUpload' do
+  describe 'UploadBase64' do
+    it 'should take a base64 image and decode it' do
+      time = Time.now.to_s.delete(' ')
+      base64_image = File.read('spec/files/base64_image.txt')
+      file_name = "#{time}-image.jpg"
+      image_link = ImageUpload.upload_base64(base64_image,file_name)
 
-  describe 'upload' do
+      base64_text = Base64.encode64(open(image_link) { |io| io.read })
+      ImageUpload.delete(file_name)
+
+      expect(base64_text.gsub("\n",'')).to eq(base64_image.gsub("\n",''))
+
+    end
+  end
+
+  describe 'Upload' do
 
     it 'should upload to s3 bucket' do
-      @text = ''
-      @time = Time.now.to_s.delete(' ')
-      @image_link = ImageUpload.upload('spec/files/image.txt', "#{@time}-image.txt")
+      file_name = Time.now.to_s.delete(' ') + '-image.txt'
+      file_path = 'spec/files/image.txt'
+      image_link = ImageUpload.upload(file_path, file_name)
 
-      open(@image_link) do |image|
+      text = ''
+      open(image_link) do |image|
         image.each_line do |line|
-          @text += line
+          text += line
         end
       end
 
-      expect(@text).to eq('Testy test.')
+      ImageUpload.delete(file_name)
+
+      expect(text).to eq('Testy test.')
     end
 
     it 'should throw an exception on invalid file path' do
