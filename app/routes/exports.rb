d86be97b2604f27fa1ae4e1a4acb53677ccdec6f
Claire Dodd <cdodd@thoughtworks.com>
2015-08-12 15:26:34 +1000

    Moved exports?users to exports/users and same for reports

--- app/routes/exports.rb
+++ app/routes/exports.rb
@@ -3,29 +3,37 @@ require_relative '../models/user'
 require 'csv'
 require 'zip'
 
-get '/exports/?' do
+get '/exports/users?' do
 
-  permitted = %w(users reports)
+  permitted = %w()
   required = %w()
 
   return status 400 unless validate_params?(params, permitted, required)
   return status 401 unless warden.authenticated?
 
   content_type 'application/csv'
-  if params[:users]
-    attachment 'users-encrypted.zip'
-    encrypt_with = ENV['USER_ENCRYPTION_PASSWORD']
-    encrypt_with = 'test' if ENV['RACK_ENV']=='test'
-    return Zip::OutputStream.write_buffer(::StringIO.new(''), Zip::TraditionalEncrypter.new(encrypt_with)) do |out|
-      out.put_next_entry('users.csv')
-      out.write User.export
-    end.string
-    return
-  else
-    if params[:reports]
-      attachment 'reports.csv'
-      reports = Report.export
-      return reports
-    end
+
+  attachment 'users-encrypted.zip'
+  encrypt_with = ENV['USER_ENCRYPTION_PASSWORD']
+  encrypt_with = 'test' if ENV['RACK_ENV']=='test'
+  return Zip::OutputStream.write_buffer(::StringIO.new(''), Zip::TraditionalEncrypter.new(encrypt_with)) do |out|
+    out.put_next_entry('users.csv')
+    out.write User.export
+  end.string
+  return
+end
+get '/exports/reports?' do
+  permitted = %w()
+  required = %w()
+
+  return status 400 unless validate_params?(params, permitted, required)
+  return status 401 unless warden.authenticated?
+
+  content_type 'application/csv'
+  if params[:reports]
+    attachment 'reports.csv'
+    reports = Report.export
+    return reports
   end
 end
+
--- spec/routes/exports_spec.rb
+++ spec/routes/exports_spec.rb
@@ -1,29 +1,48 @@
 describe 'Exports' do
   describe 'get' do
-    describe '/exports?users=true' do
+    describe '/exports/users' do
 
       describe 'when not logged in' do
         describe 'should return status' do
-        it '401 given users' do
-          get '/exports?users=true'
-          expect(last_response.status).to be(401)
-        end
-        it '400 when given unknown params' do
-          get '/exports?hello=true'
-          expect(last_response.status).to be(400)
-        end
-        it '400 when given unknown and correct params' do
-          get '/exports?users=wow&notagoodparam'
-          expect(last_response.status).to be(400)
-        end
+          it '401 given users' do
+            get '/exports/users'
+            expect(last_response.status).to be(401)
           end
+          it '400 when given unknown params' do
+            get '/exports/users?hello=true'
+            expect(last_response.status).to be(400)
+          end
+        end
       end
       describe 'when logged in' do
         before(:each) do
           login_as :Admin
         end
         it 'should return status 200 when logged in' do
-          get '/exports?users=true'
+          get '/exports/users'
+          expect(last_response.status).to be(200)
+        end
+      end
+    end
+    describe '/exports/reports' do
+      describe 'when not logged in' do
+        describe 'should return status' do
+          it '401 given users' do
+            get '/exports/reports'
+            expect(last_response.status).to be(401)
+          end
+          it '400 when given unknown params' do
+            get '/exports/reports?hello=true'
+            expect(last_response.status).to be(400)
+          end
+        end
+      end
+      describe 'when logged in' do
+        before(:each) do
+          login_as :Admin
+        end
+        it 'should return status 200 when logged in' do
+          get '/exports/reports'
           expect(last_response.status).to be(200)
         end
       end
